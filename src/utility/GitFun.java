d0570999290357a672d21272b4ffe6bb7dd3bdb2
JosueValladares <68883897+JosueValladares@users.noreply.github.com>
2020-08-05 21:33:48 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,6 +3,6 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Keep pushing until you get offers");
-        System.out.println("Do not stop, Keep moving foward")
+        System.out.println("Do not stop, Keep moving foward");
     }
 }
